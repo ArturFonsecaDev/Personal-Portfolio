b1b8ec15be42b6224fa3960d4b5ba9114cb50ed8
trdntb <130498186+trdntb@users.noreply.github.com>
2024-04-11 01:08:45 -0300

    doc formatted

--- recipes/views.py
+++ recipes/views.py
@@ -4,7 +4,9 @@ from django.http import HttpResponseRedirect
 
 # Create your views here.
 
-def home_page(req): return render(req, 'recipes/index.html' )
+
+def home_page(req): return render(req, 'recipes/index.html')
+
 
 def processing_forms(req):
     if req.method == 'POST':
@@ -13,10 +15,10 @@ def processing_forms(req):
         msg = req.POST.get('message')
         send_mail(
             f'Subject: Thanks for contacting. Your message just arrived at my mail box, in no time you will get an answer!',
-                f'I read your following request, and will answer soon, thanks for the time!\n{msg}',
-                'sgsartur@hotmail.com',  # Usar o email do remetente
-                [email],  # Enviar para o email do usuário
-                fail_silently=False,
+            f'I read your following request, and will answer soon, thanks for the time!\n{msg}',
+            'sgsartur@hotmail.com',  # Usar o email do remetente
+            [email],  # Enviar para o email do usuário
+            fail_silently=False,
         )
         send_mail(
             f'{name} wanna discuss about a project',
